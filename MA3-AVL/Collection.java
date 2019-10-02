628c7fb18e80839c9b163f6d964e254c3fcb1b2d
acrandal <acrandal@eecs.wsu.edu>
2019-10-01 22:21:04 -0700

    MA3 merge to master

---
+++ MA3-AVL/Collection.java
@@ -0,0 +1,20 @@
+/*
+ *  Microassignment: Avl Tree Rotations
+ *
+ *  Collection.java: A simple Collection API to inherit
+ * 
+ *  Contributors:
+ *    Bolong Zeng <bzeng@wsu.edu>, 2018
+ *    Aaron S. Crandall <acrandal@wsu.edu>, 2019
+ * 
+ *  Copyright:
+ *   For academic use only under the Creative Commons
+ *   Attribution-NonCommercial-NoDerivatives 4.0 International License
+ *   http://creativecommons.org/licenses/by-nc-nd/4.0
+ */
+
+public abstract class Collection<T> {
+	public abstract void addElement(T item);
+	public abstract boolean isEmpty();
+	public abstract int getSize();
+}
